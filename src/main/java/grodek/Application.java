ca9ba3af09d1c10cb1e92c38da223095c6d88510
Mariusz <mariusz.grodek@gmail.com>
2015-06-11 00:57:19 +0200

    Adding simple REST controller and basic gradle configuration

---
+++ src/main/java/grodek/Application.java
@@ -0,0 +1,12 @@
+package grodek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
